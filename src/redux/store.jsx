de96bc24f56615009719e5c6d754a9aa19f09e5e
saurabh <saurabh2113076@akgec.ac.in>
2025-01-11 01:09:34 +0530

    some changes in dependency

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,16 +1,19 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
-import { thunk } from 'redux-thunk';
+import thunk from 'redux-thunk'; // Corrected import
 import { composeWithDevTools } from 'redux-devtools-extension';
 import authReducer from './reducers/authReducer';
 import taskReducer from './reducers/taskReducer';
 
+// Combine all reducers
 const rootReducer = combineReducers({
   auth: authReducer,
   tasks: taskReducer,
 });
 
+// Apply middleware (thunk)
 const middleware = [thunk];
 
+// Create the Redux store with dev tools
 const store = createStore(
   rootReducer,
   composeWithDevTools(applyMiddleware(...middleware))
