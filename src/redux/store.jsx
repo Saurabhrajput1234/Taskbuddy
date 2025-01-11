f9caaa3ea4e4a4a6269a8fba9cf691462d648d71
saurabh <saurabh2113076@akgec.ac.in>
2025-01-11 20:34:28 +0530

    make some changes in styte

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,19 +1,16 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk'; // Corrected import
+import thunk from 'redux-thunk'; 
 import { composeWithDevTools } from 'redux-devtools-extension';
 import authReducer from './reducers/authReducer';
 import taskReducer from './reducers/taskReducer';
 
-// Combine all reducers
 const rootReducer = combineReducers({
   auth: authReducer,
   tasks: taskReducer,
 });
 
-// Apply middleware (thunk)
 const middleware = [thunk];
 
-// Create the Redux store with dev tools
 const store = createStore(
   rootReducer,
   composeWithDevTools(applyMiddleware(...middleware))
